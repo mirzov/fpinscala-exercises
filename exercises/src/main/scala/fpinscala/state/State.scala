eccf5d4555ac38d03641f149b38c4c0fa44e9996
Oleg Mirzov <oleg@everest.(none)>
2012-11-05 22:18:26 +0100

    State: did exercises up to #8 inclusive

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -28,23 +28,70 @@ object RNG {
       (f(a), rng2)
     }
 
-  def positiveInt(rng: RNG): (Int, RNG) = sys.error("todo")
+  def positiveInt(rng: RNG): (Int, RNG) = {
+		val (i,r) = rng.nextInt
+		if(i == Int.MinValue) positiveInt(r) else (i.abs,r)
+	}
 
-  def double(rng: RNG): (Double, RNG) = sys.error("todo")
+  def double(rng: RNG): (Double, RNG) = {
+		val (i,r) = positiveInt(rng)
+		(i.toDouble / Int.MaxValue, r)
+	}
 
-  def intDouble(rng: RNG): ((Int,Double), RNG) = sys.error("todo")
+  def intDouble(rng: RNG): ((Int,Double), RNG) = {
+		val (i,r1) = rng.nextInt
+		val (d, r2) = double(r1)
+		((i,d),r2)
+	}
 
-  def doubleInt(rng: RNG): ((Double,Int), RNG) = sys.error("todo")
+  def doubleInt(rng: RNG): ((Double,Int), RNG) = {
+		intDouble(rng) match {
+			case ((i,d),r) => ((d,i),r)
+		}
+	}
 
-  def double3(rng: RNG): ((Double,Double,Double), RNG) = sys.error("todo")
+  def double3(rng: RNG): ((Double,Double,Double), RNG) = {
+		val (d1,r1) = double(rng)
+		val (d2,r2) = double(r1)
+		val (d3,r3) = double(r2)
+		((d1, d2, d3),r3)
+	}
 
-  def ints(count: Int)(rng: RNG): (List[Int], RNG) = sys.error("todo")
+  def ints(count: Int)(rng: RNG): (List[Int], RNG) = {
+		if(count <= 0) (Nil, rng)
+		else ints(count - 1)(rng) match{
+			case (ilist,r) =>
+				val (inext, rnext) = r.nextInt
+				(inext :: ilist, rnext)
+		}
+	}
 
-  def positiveMax(n: Int): Rand[Int] = sys.error("todo")
+  def positiveMax(n: Int): Rand[Int] = map(int)(i => i % (n + 1))
 
-  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = sys.error("todo")
+	val double_elegant: Rand[Double] = map(int)(i => i.toDouble / Int.MaxValue)
 
-  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = sys.error("todo")
+  def map2[A,B,C](ra: Rand[A], rb: Rand[B])(f: (A, B) => C): Rand[C] = { rnd =>
+		val (a, r1) = ra(rnd)
+		val (b, r2) = rb(r1)
+		(f(a,b), r2)
+	}
+
+	val intDouble_elegant: Rand[(Int,Double)] = map2(int, double_elegant)((i,d) => (i,d))
+
+	val doubleInt_elegant: Rand[(Double,Int)] = map2(int, double_elegant)((i,d) => (d,i))
+
+  def sequence[A](fs: List[Rand[A]]): Rand[List[A]] = {
+		fs match {
+			case Nil => rng => (Nil, rng)
+			case fhead :: frest => rng => {
+				val (ahead, r1) = fhead(rng)
+				val (arest, rfinal) = sequence(frest)(r1)
+				(ahead :: arest, rfinal)
+			}
+		}		
+	}
+
+	def ints_elegant(n: Int): Rand[List[Int]] = sequence((1 to n).map(_ => int).toList)
 
   def flatMap[A,B](f: Rand[A])(g: A => Rand[B]): Rand[B] = sys.error("todo")
 }
