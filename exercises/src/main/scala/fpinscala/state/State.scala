c11e784855d97d6ba7511fe9d34812b5d4bfb799
Oleg Mirzov <oleg.mirzov@gmail.com>
2012-11-07 17:58:46 +0100

    Started excercise 11

--- exercises/src/main/scala/fpinscala/state/State.scala
+++ exercises/src/main/scala/fpinscala/state/State.scala
@@ -110,12 +110,24 @@ object RNG {
 }
 
 case class State[S,+A](run: S => (A, S)) {
-  def map[B](f: A => B): State[S, B] =
-    sys.error("todo")
-  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] =
-    sys.error("todo")
-  def flatMap[B](f: A => State[S, B]): State[S, B] =
-    sys.error("todo")
+
+  def map[B](f: A => B): State[S, B] = flatMap((State.unit[S, B] _).compose(f))
+//  State(s => {
+//	val (a, s1) = run(s)
+//	(f(a), s1)
+//  })
+  
+  def map2[B,C](sb: State[S, B])(f: (A, B) => C): State[S, C] = State(s => {
+	val (a, s1) = run(s)
+	val (b, s2) = sb.run(s1)
+	(f(a,b), s2)
+  })
+  
+  def flatMap[B](f: A => State[S, B]): State[S, B] = State(s => {
+	val (a, s1) = run(s)
+	f(a).run(s1)
+  })
+  
 }
 
 sealed trait Input
@@ -125,6 +137,9 @@ case object Turn extends Input
 case class Machine(locked: Boolean, candies: Int, coins: Int)
 
 object State {
+
+  def unit[S,A](a: A):State[S, A] = State(s => (a, s))
+  
   type Rand[A] = State[RNG, A]
   def simulateMachine(inputs: List[Input]): State[Machine, Int] = sys.error("todo")
 }
