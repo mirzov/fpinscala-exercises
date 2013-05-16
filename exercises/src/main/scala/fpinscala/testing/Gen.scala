81abe4aabd7c0e636d6192bc946ec58e33ce3058
Oleg Mirzov <oleg.mirzov@gmail.com>
2013-05-16 23:14:31 +0200

    Ex. 6 in 'Testing', added Eclipse plugin

--- exercises/src/main/scala/fpinscala/testing/Gen.scala
+++ exercises/src/main/scala/fpinscala/testing/Gen.scala
@@ -44,27 +44,41 @@ object Gen {
 		(sample, r1)
 	}
 
-	def unit[A](a: => A): Gen[A] = Gen(State.unit(a), Stream(a))
+	def unit[A](a: => A): Gen[A] = Gen(State.unit(a), Stream(Some(a)))
 
-	def boolean: Gen[Boolean] = Gen(State(RNG.int).map(_ % 2 == 0), Stream(false,true))
+	def boolean: Gen[Boolean] = Gen(State(RNG.int).map(_ % 2 == 0), Stream(Some(false),Some(true)))
 
 	def choose(start: Int, stopExclusive: Int): Gen[Int] = Gen(
 		chooseRNG(start, stopExclusive),
-		Stream.from(start).take(stopExclusive - start)
+		Stream.from(start).take(stopExclusive - start).map(Some(_))
 	)
 
 	def listOfN[A](n: Int, g: Gen[A]): Gen[List[A]] = {
 		val listSample = State.sequence(List.fill(n)(g.sample))
-		def getStreamOfLists(len: Int): Stream[List[A]] =
-			if(len <= 0) g.exhaustive.map(_ => Nil)
-			else if(len == 1) g.exhaustive.map(List(_))
-			else for(a <- g.exhaustive; l <- getStreamOfLists(len - 1)) yield a :: l
+		def getStreamOfLists(len: Int): Stream[Option[List[A]]] =
+			if(len <= 0) g.exhaustive.map(_ => Some(Nil))
+			else if(len == 1) g.exhaustive.map(intOp => intOp.map(List(_)))
+			else for(
+			    aopt <- g.exhaustive;
+			    if aopt.isDefined;
+			    a = aopt.get;
+			    lopt <- getStreamOfLists(len - 1);
+			    if lopt.isDefined;
+			    l = lopt.get
+			) yield Some(a :: l)
 		Gen(listSample, getStreamOfLists(n))
 	}
+	
+	def uniform: Gen[Double] = Gen(
+	    chooseRNG(0, Int.MaxValue).map(_.asInstanceOf[Double] / Int.MaxValue),
+	    Stream(None)
+	)
+
+	def choose(i: Double, j: Double): Gen[Double] = uniform.map[Double, Double](u => i + (j - i) * u)
 
 }
 
-case class Gen[+A](sample: State[RNG,A], exhaustive: Stream[A]){
+case class Gen[+A](sample: State[RNG,A], exhaustive: Stream[Option[A]]){
 	def map[A,B](f: A => B): Gen[B] = sys.error("placeholder")
 	def flatMap[A,B](f: A => Gen[B]): Gen[B] = sys.error("placeholder")
 }
